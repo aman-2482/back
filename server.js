199ddd3e9a237ba5847d6760ba79f57b756264e5
aman-2482 <amanc273014@gmail.com>
2024-01-05 14:14:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/aman', (req, res) => {
+    res.send('Hello aman ')
+  })
+app.get('/you', (req, res) => {
+    res.send('Hello am ')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
